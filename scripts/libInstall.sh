901dcd40de234f26358676664cdb764a1ca0992f
Colin Zeidler <c.zeidler.wc@gmail.com>
2016-11-01 19:44:57 -0400

    added python requests to the python libraries

--- scripts/libInstall.sh
+++ scripts/libInstall.sh
@@ -4,8 +4,8 @@ echo "apt installing python-pip, python-dev (to compile python libraries), and g
 
 sudo apt-get install python-pip python-dev gcc
 
-echo "Installing pysnmp and psutil"
-`/usr/bin/pip -q install pysnmp psutil` && echo "pysnmp and psutil installed"
+echo "Installing pysnmp psutil and requests"
+`/usr/bin/pip -q install pysnmp psutil requests` && echo "pysnmp and psutil installed"
 
 if [ "$1" == "Manager" ]; then
 	echo "Installing flask"
