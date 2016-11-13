20f70c6e68a153b91eda252705db9f2fac681e22
Colin Zeidler <c.zeidler.wc@gmail.com>
2016-11-13 16:26:27 -0500

    fix for current d3 version

--- nodeManage/static/app.js
+++ nodeManage/static/app.js
@@ -1,46 +1,10 @@
 $(document).ready(function(){
-	var width = 960;
-	var height = 500;
+	var svg = d3.select("svg");
+	var width = +svg.attr("width");
+	var height = +svg.attr("height");
 
 	var links = [];
 	var nodes = [];
-	
-	var force = d3.layout.force()
-		.nodes(nodes)
-		.links(links)
-		.charge(-400)
-		.linkDistance(120)
-		.size([width, height])
-		.on("tick", tick);
-
-	var svg = d3.select("#map").append("svg")
-		.attr("width", width)
-		.attr("height", height);
-
-	var node = svg.selectAll(".node");
-	var link = svg.selectAll(".link");
-
-	function tick() {
-		node.attr("cx", function(d) { return d.x; })
-			.attr("cy", function(d) { return d.y; })
-
-		link.attr("x1", function(d) { return d.source.x; })
-			.attr("y1", function(d) { return d.source.y; })
-			.attr("x2", function(d) { return d.target.x; })
-			.attr("y2", function(d) { return d.target.y; })
-	}
-
-	function start() {
-		link = link.data(force.links(), function(d) { return d.source.id + "-" + d.target.id; });
-		link.enter().insert("line", ".node").attr("class", "link");
-		link.exit().remove();
-
-		node = node.data(force.nodes(), function(d) { return d.id; });
-		node.enter().append("circle").attr("class", "node").attr("r", 30);
-		node.exit().remove();
-
-		force.start();
-	}
 		
 	$.get("/nodes", function(data) {
 		nodes.push(JSON.parse(data))
@@ -55,4 +19,39 @@ $(document).ready(function(){
 	$("button").click(function() {
 
 	});
+	
+	var sim = d3.forceSimulation()
+		.force("link", d3.forceLink().id(function(d) { return d.id; }))
+		.force("charge", d3.forceManyBody())
+		.force("center", d3.forceCenter(width/2, height/2));
+
+
+	var node = svg.append("g")
+		.attr("class", "nodes")
+		.selectAll("circle")
+		.data(nodes)
+		.enter().append("circle")
+			.attr("r", 30)
+			.attr("class", "node");
+
+	var link = svg.append("g")
+		.attr("class", "links")
+		.selectAll("line")
+		.data(links)
+		.enter().append("line")
+			.attr("class", "link");
+
+	sim.nodes(nodes).on("tick", tick);
+	sim.force("link").links(links);
+
+	function tick() {
+		node.attr("cx", function(d) { return d.x; })
+			.attr("cy", function(d) { return d.y; })
+
+		link.attr("x1", function(d) { return d.source.x; })
+			.attr("y1", function(d) { return d.source.y; })
+			.attr("x2", function(d) { return d.target.x; })
+			.attr("y2", function(d) { return d.target.y; })
+	}
+
 });
