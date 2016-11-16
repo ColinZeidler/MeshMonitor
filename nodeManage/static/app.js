27b82d4bff55f617808c09e5f169dd3b82e6bd6f
Colin Zeidler <c.zeidler.wc@gmail.com>
2016-11-16 14:22:07 -0500

    Update app.js

--- nodeManage/static/app.js
+++ nodeManage/static/app.js
@@ -1,36 +1,33 @@
-$(document).ready(function(){
-	var svg = d3.select("svg");
-	var width = +svg.attr("width");
-	var height = +svg.attr("height");
-	
-	var sim = d3.forceSimulation()
-		.force("link", d3.forceLink().id(function(d) { return d.id; }))
-		.force("charge", d3.forceManyBody())
-		.force("center", d3.forceCenter(width/2, height/2));
-	
+var svg = d3.select("svg");
+var width = +svg.attr("width");
+var height = +svg.attr("height");
+
+var sim = d3.forceSimulation()
+	.force("link", d3.forceLink().id(function(d) { return d.id; }))
+	.force("charge", d3.forceManyBody())
+	.force("center", d3.forceCenter(width/2, height/2));
+
+d3.json("/nodes", function(error, n) {
+	d3.json("/topology", function(error, t) {
+	console.log(n)
+	console.log(t)
 	var node = svg.append("g")
 		.attr("class", "nodes")
-		.selectAll("circle");
+		.selectAll("circle")
+		.data(n)
+		.enter().append("circle")
+			.attr("r", 30)
+			.attr("class", "node");
 	var link = svg.append("g")
 		.attr("class", "links")
-		.selectAll("line");
-	d3.json("/nodes", function(error, n) {
-			node.data(n)
-			.enter().append("circle")
-				.attr("r", 30)
-				.attr("class", "node");
+		.selectAll("line")
+		.data(t)
+		.enter().append("line")
+			.attr("class", "link").on("click", node_click);
+
 		sim.nodes(n).on("tick", tick);
-	});
-	d3.json("/topology", function(error, t) {
-			link.data(t)
-			.enter().append("line")
-				.attr("class", "link");
 		sim.force("link").links(t);
-	});
-	$("button").click(function() {
-
-	});
-
+		
 	function tick() {
 		node.attr("cx", function(d) { return d.x; })
 			.attr("cy", function(d) { return d.y; })
@@ -40,5 +37,10 @@ $(document).ready(function(){
 			.attr("x2", function(d) { return d.target.x; })
 			.attr("y2", function(d) { return d.target.y; })
 	}
-
+	});
 });
+
+
+function node_click() {
+	alert(function(d) { return d.name });
+}
